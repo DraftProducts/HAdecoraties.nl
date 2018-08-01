10eaa6c64c8d0483c6cfd1e72e862b8b84d1494b
DraftMan <nicovanaarsen@gmail.com>
2018-08-01 16:47:43 +0200

    async/await added

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -1,6 +1,9 @@
 'use strict'
 
 const { get, post } = require('snekfetch');
+const Mail = use('Mail')
+const { validate } = use('Validator')
+
 const album = '694485600890626'
 const appId = '1862844164021508'
 const appSecret = 'f5c8d3957b08592c5ef985d90e1ab733'
@@ -9,14 +12,10 @@ class HomeController {
     async index({ view }){
         //https://graph.facebook.com/694485600890626?fields=photos{link}&access_token=EAADrnh9xgQ8BAOWQ837n6ZBkSSMtfYBZC6sfSYiZCu4ogJ6FpOYIJPtbZCpFUntcrbjrrB7H1SKAwVGrde3ZBGsBVb5ZAhnFufCTUyvQZCvv7hROMqawUr3O49189EWxg1WxcQ2klJMrBPRt9H92cIMGl2PZA8r5eYubl7UTkcGGnysRZC0KZChEnV5ZCWxWZA3ttOftS9oBxztPgQZDZD
         
-        get(`https://graph.facebook.com/oauth/access_token?client_id=${appId}&client_secret=${appSecret}&grant_type=client_credentials`)
-        .then(res => {
-            return get(`https://graph.facebook.com/${album}/photos?fields=link&access_token=${res.body.access_token}`)
-            .then(response => {
-                const links = JSON.parse(response.body.toString('utf8'))
-                view.render('index', {links}) 
-            })
-        })
+        const res = await get(`https://graph.facebook.com/oauth/access_token?client_id=${appId}&client_secret=${appSecret}&grant_type=client_credentials`)
+        const response = await get(`https://graph.facebook.com/${album}/photos?fields=images,link&access_token=${res.body.access_token}`)
+        const producten = JSON.parse(response.body.toString('utf8'))
+        return view.render('index', {producten}) 
     }
 
     async mail ({ request, session, response }) {
@@ -42,7 +41,7 @@ class HomeController {
             .withErrors(validation.messages())
             .flashExcept()
 
-            return response.redirect('back')
+            return response.redirect('/#contact')
         }
     
         const email_to = 'hadecoraties@gmail.com';
