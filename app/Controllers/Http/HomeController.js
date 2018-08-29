34e1819e9450b8d9575e0233cca4812a5c82276f
DraftMan <nicovanaarsen@gmail.com>
2018-08-29 11:58:17 +0200

    some thing is modified

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -11,11 +11,11 @@ const appSecret = 'f5c8d3957b08592c5ef985d90e1ab733'
 class HomeController {
     async index({ view }){
         //https://graph.facebook.com/694485600890626?fields=photos{link}&access_token=EAADrnh9xgQ8BAOWQ837n6ZBkSSMtfYBZC6sfSYiZCu4ogJ6FpOYIJPtbZCpFUntcrbjrrB7H1SKAwVGrde3ZBGsBVb5ZAhnFufCTUyvQZCvv7hROMqawUr3O49189EWxg1WxcQ2klJMrBPRt9H92cIMGl2PZA8r5eYubl7UTkcGGnysRZC0KZChEnV5ZCWxWZA3ttOftS9oBxztPgQZDZD
-        
+
         const res = await get(`https://graph.facebook.com/oauth/access_token?client_id=${appId}&client_secret=${appSecret}&grant_type=client_credentials`)
         const response = await get(`https://graph.facebook.com/${album}/photos?fields=images,link&access_token=${res.body.access_token}`)
         const producten = JSON.parse(response.body.toString('utf8'))
-        return view.render('index', {producten}) 
+        return view.render('index', {producten})
     }
 
     async mail ({ request, session, response }) {
@@ -33,7 +33,7 @@ class HomeController {
           tel: 'required',
           message: 'required'
         }
-    
+
         const validation = await validate(request.all(), rules, messages)
 
         if (validation.fails()) {
@@ -43,10 +43,10 @@ class HomeController {
 
             return response.redirect('/#contact')
         }
-    
+
         const email_to = 'hadecoraties@gmail.com';
         const data = request.only(['email','name','tel','message'])
-    
+
         await Mail.send('mails/contact', data, (message) => {
           message
             .to(email_to)
